aa6740c77fa8671bb2328ecff151bf775228f683
Corentin COUTRET-ROZET <corentin.rozet@epitech.eu>
2021-01-05 11:53:25 +0100

    [ADD] final display and parsing

--- sources/Multigrains.py
+++ sources/Multigrains.py
@@ -16,6 +16,41 @@ class Multigrains():
 
     def __init__(self):
         self._total = 0
+        self._grains = {'Oat':      [1, 1, 2, 0],
+                        'Wheat':    [0, 2, 1, 0],
+                        'Corn':     [1, 0, 0, 3],
+                        'Barley':   [0, 1, 1, 1],
+                        'Soy':      [2, 0, 0 , 2]}
+        self._ressources = [0, 0, 0, 0]
+        self._prices = [0, 0, 0, 0, 0]
+        self._production = [0, 0, 0, 0, 0]
+
+    def parse(self, argv: list) -> None:
+
+        """Parse the input arguments."""
+
+        self._ressources[0] = int(argv[1])
+        self._ressources[1] = int(argv[2])
+        self._ressources[2] = int(argv[3])
+        self._ressources[3] = int(argv[4])
+        self._prices[0] = int(argv[5])
+        self._prices[1] = int(argv[6])
+        self._prices[2] = int(argv[7])
+        self._prices[3] = int(argv[8])
+        self._prices[4] = int(argv[9])
+
+    def display(self) -> None:
+
+        def displayProduction(i=0) -> None:
+            for grain in self._grains:
+                print(f"{grain}: {self._production[i]} units at ${self._prices[i]}/unit")
+                i += 1
+
+        print(f"Resources: {self._ressources[0]} F1, {self._ressources[1]} F2, {self._ressources[2]} F3, {self._ressources[3]} F4")
+        print()
+        displayProduction()
+        print()
+        print("Total production value: ${:.2f}".format(self._total))
 
     def run(self, argv: list) -> None:
 
@@ -23,4 +58,6 @@ class Multigrains():
         Run computations and process output printing.
         """
 
-        return 0
+        self.parse(argv)
+        # self.compute()
+        self.display()
