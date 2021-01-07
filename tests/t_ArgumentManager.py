2d49d705967995a830e7307fe0185e28c7633fa8
Corentin COUTRET-ROZET <corentin.rozet@epitech.eu>
2021-01-07 15:57:27 +0100

    [ADD] tests: Args Manager : check args

--- tests/t_ArgumentManager.py
+++ tests/t_ArgumentManager.py
@@ -13,13 +13,47 @@ import pytest
 from sources.ArgumentManager import ArgumentManager
 
 
-# def test_ok_1_arguments(capsys):
+def test_arguments_ok():
 
-#     argMan = ArgumentManager()
+    argMan = ArgumentManager()
 
-#     argv = ['./307multigrains', 'Makefile']
+    argv = ['./307multigrains', '45', '41', '21', '63', '198', '259', '257', '231', '312']
 
-#     assert argMan.checkArgs(argv) == 0
+    assert argMan.checkArgs(argv) != 84
+
+
+def test_wrong_number_args(capsys):
+
+    argMan = ArgumentManager()
+
+    argv = ['./307multigrains', '45', '21', '63', '198', '259', '257', '231', '312']
+    assert argMan.checkArgs(argv) == 84
+
+    redir = capsys.readouterr()
+    assert redir.out == "ERROR: wrong number of arguments.\n"
+
+
+def test_not_digit(capsys):
+
+    argMan = ArgumentManager()
+
+    argv = ['./307multigrains', '45', 'a', '21', '63', '198', '259', '257', '231', '312']
+    assert argMan.checkArgs(argv) == 84
+
+    redir = capsys.readouterr()
+    assert redir.out == "ERROR: arguments might be integers.\n"
+
+
+def test_negative_value(capsys):
+
+    argMan = ArgumentManager()
+
+    argv = ['./307multigrains', '45', '-3838', '21', '63', '198', '259', '257', '231', '312']
+    assert argMan.checkArgs(argv) == 84
+
+    redir = capsys.readouterr()
+    assert redir.out == "ERROR: arguments might be positive values.\n"
+
 
 def test_needHelp_h():
 
